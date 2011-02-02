811ea17a491c686f2b05a997ffecf346092e0392
Brian Watling <brianwatling@hotmail.com>
2011-02-01 21:59:26 -0800

    use AF_INET

--- src/ServerSocket.cpp
+++ src/ServerSocket.cpp
@@ -34,7 +34,7 @@ ServerSocket::ServerSocket(const std::string& host, const std::string& port)
     struct addrinfo* res;
 
     memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_UNSPEC;// use IPv4 or IPv6, whichever
+    hints.ai_family = AF_INET;// use IPv4 or IPv6, whichever
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_flags = AI_PASSIVE;// fill in my IP for me
     if(getaddrinfo(host.c_str(), port.c_str(), &hints, &res))
